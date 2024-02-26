4d4f3a270d1f50739bb2a874fc82850f098a3929
Ludovic THUARD <ludovic.thuard@sogeti.com>
2024-02-26 08:54:59 +0100

    Install and setupd swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('sogeti_genAI_project')
+    .setDescription('Simple Nodes.js/Nest API generated with Github Copilot')
+    .setVersion('1.0')
+    .addTag('nestjs')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
